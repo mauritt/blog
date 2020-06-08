1d0eba67177cfa56fdae6a9c60c04a5041eb7cc8
Michael Auritt <michael@michaelauritt.com>
2020-06-08 15:39:27 -0400

    Added function for getting all posts

--- blogPostAPI/internal/database/database.go
+++ blogPostAPI/internal/database/database.go
@@ -7,6 +7,13 @@ import (
 	"os"
 )
 
+// BlogPost containts the data for blog posts
+type BlogPost struct {
+	ID       int    `json:"id"`
+	Headline string `json:"headline"`
+	Content  string `json:"content"`
+}
+
 // ConnectDB connects to the database
 func ConnectDB() (*sql.DB, error) {
 
@@ -30,4 +37,44 @@ func ConnectDB() (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+// GetBlogPosts gets all posts
+func GetBlogPosts(db *sql.DB) ([]BlogPost, error) {
+
+	var post BlogPost
+	var posts []BlogPost
+
+	query := "SELECT * FROM post"
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Printf("Couldn't prepare statement: %v\v%v", query, err.Error())
+		return nil, err
+	}
+
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Printf("Query failed\n%v", err.Error())
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&post.ID, &post.Headline, &post.Content)
+		if err != nil {
+			log.Printf(err.Error())
+			return nil, err
+		}
+		posts = append(posts, post)
+
+	}
+	err = rows.Err()
+	if err = rows.Err(); err != nil {
+		log.Printf(err.Error())
+		return nil, err
+	}
+
+	return posts, nil
+
 }
